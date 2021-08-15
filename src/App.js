9af1bdf5040f595533232d8a842e817eb688599e
Thiago <thiagodesouza.dev@gmail.com>
2021-08-15 18:51:47 -0300

    finalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './components/StrictAccess';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/users">Users</Link></li>
+              <li><Link to="/strictAccess">StrictAcess</Link></li>
+            </ul>
+          </nav>
+          <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/users/:id" render={(props) => <Users {...props} greetingsMessage={'Good Morning'}/>} />
+      <Route path="/about" component={About}/>
+      <Route path="/strictAccess" render={ () => (
+        <StrictAccess user={ { username: 'joao', password: '1234' } } />
+        )}
+        />
+          </Switch>
+      </BrowserRouter>
     );
   }
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const {
+      greetingMessage = 'Hi There',
+      match: { params: { id }
+    } } = this.props;
+
     return (
-      <div>
-        <h2>Users</h2>
-        <p> My awesome Users component </p>
-      </div>
+      <main>
+        <h2> Users </h2>
+        <p>
+        { `${greetingMessage} User ${id}` }, this is my awesome Users component
+        </p>
+      </main>
     );
   }
-};
+}
 
 export default Users;
